b298d814b990874ae0f5bb33ca504cdb1f421f79
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2017-07-22 08:56:00 +0200

    Allows `wheel` group to manage certain services.

--- etc/nixos/users-configuration.nix
+++ etc/nixos/users-configuration.nix
@@ -3,4 +3,11 @@
     isNormalUser = true;
     extraGroups = ["docker" "networkmanager" "vboxusers" "wheel"];
   };
+
+  security.sudo.extraConfig =
+    ''
+      %wheel ALL=NOPASSWD: /run/current-system/sw/bin/systemctl start display-manager
+      %wheel ALL=NOPASSWD: /run/current-system/sw/bin/systemctl restart display-manager
+      %wheel ALL=NOPASSWD: /run/current-system/sw/bin/systemctl restart openvpn-client
+    '';
 }
