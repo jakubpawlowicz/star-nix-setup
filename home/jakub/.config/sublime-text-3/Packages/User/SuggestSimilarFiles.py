820d2c8333b5607ea08443eb62dbf91bde2da314
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2017-08-19 20:51:33 +0200

    Improves `SuggestSimilarFiles` script.
    Why:
    
    * When more than one group (column) opened it goes to next right-hand group
      before showing a `goto` overlay.

--- home/jakub/.config/sublime-text-3/Packages/User/SuggestSimilarFiles.py
+++ home/jakub/.config/sublime-text-3/Packages/User/SuggestSimilarFiles.py
@@ -4,7 +4,22 @@ import sublime_plugin
 
 class SuggestSimilarFiles(sublime_plugin.TextCommand):
   def run(self, edit):
+    if "file_base_name" not in self.view.window().extract_variables():
+        return
+
     file_base_name = self.view.window().extract_variables()["file_base_name"]
+
+    self.focus_next_group()
+    self.open_goto_overlay(file_base_name)
+
+  def focus_next_group(self):
+    active_group = self.view.window().active_group()
+    num_groups = self.view.window().num_groups()
+    next_group = active_group + 1 if active_group < num_groups - 1 else 0
+
+    self.view.window().focus_group(next_group)
+
+  def open_goto_overlay(self, file_base_name):
     similar_match = re.sub(r'spec|test', '', file_base_name)
     similar_match = re.sub(r'[_\-]', ' ', similar_match)
     similar_match = similar_match.strip()
