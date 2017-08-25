83aca4d3969fe9bc28e7bd7d89710093dc2fe988
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2017-08-25 10:59:15 +0200

    Makes terminus font smaller for low-dpi screen.

--- etc/nixos/system-configuration.nix
+++ etc/nixos/system-configuration.nix
@@ -10,7 +10,7 @@
     terminus_font
   ];
 
-  i18n.consoleFont = "${pkgs.terminus_font}/share/consolefonts/ter-128n.psf.gz";
+  i18n.consoleFont = "${pkgs.terminus_font}/share/consolefonts/ter-c16n.psf.gz";
 
   system.stateVersion = "18.03";
 
