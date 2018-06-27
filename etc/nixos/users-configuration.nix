676108e69eb870193808056852248839d36747b9
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2018-06-27 10:31:44 +0200

    Uses new default ETH name.

--- etc/nixos/users-configuration.nix
+++ etc/nixos/users-configuration.nix
@@ -12,8 +12,8 @@
       %wheel ALL=NOPASSWD: /run/current-system/sw/bin/systemctl stop openvpn-client
       %wheel ALL=NOPASSWD: /run/current-system/sw/bin/systemctl restart openvpn-client
       %wheel ALL=NOPASSWD: /run/current-system/sw/bin/systemctl suspend
-      %wheel ALL=NOPASSWD: /run/current-system/sw/bin/ifconfig enp10s0 down
-      %wheel ALL=NOPASSWD: /run/current-system/sw/bin/ifconfig enp10s0 up
+      %wheel ALL=NOPASSWD: /run/current-system/sw/bin/ifconfig enp37s0 down
+      %wheel ALL=NOPASSWD: /run/current-system/sw/bin/ifconfig enp37s0 up
       %wheel ALL=NOPASSWD: /run/current-system/sw/bin/cat /sys/kernel/debug/dri/0/radeon_pm_info
     '';
 }
