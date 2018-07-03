c6b98c28360214acd2230850e1552dfcc57f39dc
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2018-07-03 08:58:11 +0200

    Bumps config to 18.03.

--- etc/nixos/system-configuration.nix
+++ etc/nixos/system-configuration.nix
@@ -5,7 +5,6 @@
     ''
       options snd_hda_intel enable=0,1
     '';
-  boot.kernelPackages = pkgs.linuxPackages_4_15;
   boot.kernelParams = [
     "hid_apple.fnmode=0"
   ];
@@ -21,7 +20,7 @@
 
   i18n.consoleFont = "${pkgs.terminus_font}/share/consolefonts/ter-128n.psf.gz";
 
-  system.stateVersion = "17.03";
+  system.stateVersion = "18.03";
 
   time.timeZone = "Europe/Warsaw";
 }
