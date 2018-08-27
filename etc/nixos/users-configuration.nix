1688573699c5e2473572ad85490b50dca7bf2e1f
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2018-08-27 08:42:09 +0200

    Removes user VPN setup.

--- etc/nixos/users-configuration.nix
+++ etc/nixos/users-configuration.nix
@@ -8,9 +8,6 @@
     ''
       %wheel ALL=NOPASSWD: /run/current-system/sw/bin/systemctl start display-manager
       %wheel ALL=NOPASSWD: /run/current-system/sw/bin/systemctl restart display-manager
-      %wheel ALL=NOPASSWD: /run/current-system/sw/bin/systemctl start openvpn-client
-      %wheel ALL=NOPASSWD: /run/current-system/sw/bin/systemctl stop openvpn-client
-      %wheel ALL=NOPASSWD: /run/current-system/sw/bin/systemctl restart openvpn-client
       %wheel ALL=NOPASSWD: /run/current-system/sw/bin/systemctl suspend
       %wheel ALL=NOPASSWD: /run/current-system/sw/bin/ifconfig enp37s0 down
       %wheel ALL=NOPASSWD: /run/current-system/sw/bin/ifconfig enp37s0 up
