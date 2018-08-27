bb5c84f5dbc935a1362464e2b9afce932b7f9f70
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2018-08-27 08:51:57 +0200

    Fixes dnsmasq setup for dev env.

--- etc/nixos/service-configuration.nix
+++ etc/nixos/service-configuration.nix
@@ -6,7 +6,7 @@
     ''
       bind-interfaces
       listen-address=127.0.0.1
-      address=/dev/127.0.0.1
+      address=/local/127.0.0.1
     '';
   services.dnsmasq.servers = [ "8.8.8.8" "4.4.4.4" ];
 
