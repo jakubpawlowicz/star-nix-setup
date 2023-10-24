81e4b639d12663a634f550cccd4cb941506c00f0
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2023-10-24 12:06:59 +0200

    fix: deprecation warning.

--- etc/nixos/service-configuration.nix
+++ etc/nixos/service-configuration.nix
@@ -9,10 +9,11 @@
       address=/int/127.0.0.1
       address=/local/127.0.0.1
     '';
-  services.dnsmasq.servers = [
+  services.dnsmasq.settings.server = [
     "1.1.1.1"
     "1.0.0.1"
     "2606:4700:4700::1111"
     "2606:4700:4700::1001"
   ];
 }
+
